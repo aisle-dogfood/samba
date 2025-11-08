6af0f7783cc552c38f935bea734ad44c6e2cffbc
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2025-11-08 23:06:09 +0000

    Apply fix for issue: Insufficiently Protected Credentials in smbencrypt.c

--- libcli/auth/smbencrypt.c
+++ libcli/auth/smbencrypt.c
@@ -125,9 +125,8 @@ bool E_deshash(const char *passwd, uint8_t p16[16])
 
 	tmpbuf = strupper_talloc(frame, passwd);
 	if (tmpbuf == NULL) {
-		/* Too many callers don't check this result, we need to fill in the buffer with something */
-		strlcpy((char *)dospwd, passwd ? passwd : "", sizeof(dospwd));
-		E_P16(dospwd, p16);
+		/* Memory allocation failed - return securely without exposing plaintext password */
+		ZERO_STRUCT(p16);
 		talloc_free(frame);
 		return false;
 	}
