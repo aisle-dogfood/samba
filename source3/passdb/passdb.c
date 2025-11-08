27a717d63755af5e12d7c445d93352ce4a9d785e
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2025-11-08 18:02:29 +0000

    Apply fix for issue: Insufficiently Protected Credentials in passdb.c

--- source3/passdb/passdb.c
+++ source3/passdb/passdb.c
@@ -2327,7 +2327,7 @@ static bool get_trust_pw_clear2(const char *domain,
 				time_t *_last_set_time,
 				char **prev_pw)
 {
-	char *pwd;
+	char *pwd = NULL;
 	time_t last_set_time;
 
 	if (cur_pw != NULL) {
@@ -2395,6 +2395,8 @@ static bool get_trust_pw_clear2(const char *domain,
 		struct timeval expire;
 
 		*cur_pw = pwd;
+		/* Clear the local pwd variable to prevent sensitive data from remaining in memory */
+		pwd = NULL;
 
 		if (account_name != NULL) {
 			*account_name = lp_netbios_name();
@@ -2419,6 +2421,8 @@ static bool get_trust_pw_clear2(const char *domain,
 		pwd = secrets_fetch_prev_machine_password(lp_workgroup());
 		if (pwd != NULL) {
 			*prev_pw = pwd;
+			/* Clear the local pwd variable to prevent sensitive data from remaining in memory */
+			pwd = NULL;
 		}
 
 		return true;
