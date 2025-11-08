999d4c5bfcfc6cd03f8b2e55dd8c06b5311e0aff
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2025-11-08 19:24:38 +0000

    Apply fix for issue: Heap Inspection in credentials_internal.h

--- auth/credentials/credentials.c
+++ auth/credentials/credentials.c
@@ -740,6 +740,7 @@ _PUBLIC_ bool cli_credentials_set_old_password(struct cli_credentials *cred,
 {
 	cred->old_password = talloc_strdup(cred, val);
 	if (cred->old_password) {
+		talloc_keep_secret(discard_const(cred->old_password));
 		/* Don't print the actual password in talloc memory dumps */
 		talloc_set_name_const(cred->old_password, "password set via cli_credentials_set_old_password");
 	}
