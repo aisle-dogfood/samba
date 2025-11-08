5618d8e9ca5e779264ba5da35488b720680d9edb
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2025-11-08 18:19:01 +0000

    Apply fix for issue: Improper Buffer Index Access in dom_sid.c

--- libcli/security/dom_sid.c
+++ libcli/security/dom_sid.c
@@ -162,6 +162,10 @@ bool dom_sid_parse_endp(const char *sidstr,struct dom_sid *sidout,
 
 	*sidout = (struct dom_sid) {};
 
+	if (sidstr == NULL || strlen(sidstr) < 2) {
+		goto format_error;
+	}
+
 	if ((sidstr[0] != 'S' && sidstr[0] != 's') || sidstr[1] != '-') {
 		goto format_error;
 	}
