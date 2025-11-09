9e6a2dc6c525ceb2439f68d21b77090d724f9602
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2025-11-09 04:40:56 +0000

    Apply fix for issue: Divide By Zero in dfree.c

--- source3/smbd/dfree.c
+++ source3/smbd/dfree.c
@@ -137,14 +137,19 @@ static uint64_t sys_disk_free(connection_struct *conn,
 	if (disk_quotas(conn, fname, &bsize_q, &dfree_q, &dsize_q)) {
 		uint64_t min_bsize = MIN(*bsize, bsize_q);
 
-		(*dfree) = (*dfree) * (*bsize) / min_bsize;
-		(*dsize) = (*dsize) * (*bsize) / min_bsize;
-		dfree_q = dfree_q * bsize_q / min_bsize;
-		dsize_q = dsize_q * bsize_q / min_bsize;
+		/* Prevent divide by zero error */
+		if (min_bsize == 0) {
+			DBG_ERR("min_bsize is zero, cannot perform quota calculations\n");
+		} else {
+			(*dfree) = (*dfree) * (*bsize) / min_bsize;
+			(*dsize) = (*dsize) * (*bsize) / min_bsize;
+			dfree_q = dfree_q * bsize_q / min_bsize;
+			dsize_q = dsize_q * bsize_q / min_bsize;
 
-		(*bsize) = min_bsize;
-		(*dfree) = MIN(*dfree,dfree_q);
-		(*dsize) = MIN(*dsize,dsize_q);
+			(*bsize) = min_bsize;
+			(*dfree) = MIN(*dfree,dfree_q);
+			(*dsize) = MIN(*dsize,dsize_q);
+		}
 	}
 
 	/* FIXME : Any reason for this assumption ? */
