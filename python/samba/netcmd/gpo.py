32749b5ca58c95013de3815bb1d44f030fad37bd
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2025-11-09 22:01:12 +0000

    Apply fix for issue: Insecure XML Parser in gpo.py

--- python/samba/netcmd/gpo.py
+++ python/samba/netcmd/gpo.py
@@ -3824,7 +3824,11 @@ samba-tool gpo manage motd set {31B2F340-016D-11D2-945F-00C04FB984F9} "Message f
             return
 
         try:
-            xml_data = ET.ElementTree(ET.fromstring(conn.loadfile(vgp_xml)))
+            # Create a secure XML parser that disables external entity processing
+            parser = ET.XMLParser()
+            parser.parser.DefaultHandler = lambda data: None
+            parser.parser.ExternalEntityRefHandler = lambda context, base, sysId, notationName: False
+            xml_data = ET.ElementTree(ET.fromstring(conn.loadfile(vgp_xml), parser))
             policysetting = xml_data.getroot().find('policysetting')
             data = policysetting.find('data')
         except NTSTATUSError as e:
