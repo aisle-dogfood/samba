24c4362c663baa6df8aed565c66794bceaf0d2a3
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2025-11-10 03:13:59 +0000

    Apply fix for issue: Clear Text Logging in cmd_drsuapi.c

--- source3/rpcclient/cmd_drsuapi.c
+++ source3/rpcclient/cmd_drsuapi.c
@@ -146,7 +146,7 @@ static void display_domain_controller_info_01(struct drsuapi_DsGetDCConnection01
 	printf("unknown4:\t%d\n", r->unknown4);
 	printf("unknown5:\t%d\n", r->unknown5);
 	printf("unknown6:\t%d\n", r->unknown6);
-	printf("client_account:\t%s\n", r->client_account);
+	printf("client_account:\t<redacted>\n");
 }
 
 static void display_domain_controller_info_1(struct drsuapi_DsGetDCInfo1 *r)
