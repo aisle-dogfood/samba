1423c2958d60f3ba6d992a1d79d3552c4fa5a1cc
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2025-11-10 02:40:04 +0000

    Apply fix for issue: Insecure XML Parser in qt5.py

--- third_party/waf/waflib/extras/pyqt5.py
+++ third_party/waf/waflib/extras/pyqt5.py
@@ -150,6 +150,11 @@ class pyrcc(Task.Task):
 			return ([], [])
 
 		parser = make_parser()
+		# Disable external entity processing to prevent XXE attacks
+		parser.setFeature("http://xml.org/sax/features/external-general-entities", False)
+		parser.setFeature("http://xml.org/sax/features/external-parameter-entities", False)
+		# Disable DTD processing
+		parser.setFeature("http://apache.org/xml/features/disallow-doctype-decl", True)
 		curHandler = XMLHandler()
 		parser.setContentHandler(curHandler)
 		fi = open(self.inputs[0].abspath(), 'r')
