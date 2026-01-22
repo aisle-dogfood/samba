e624d8ace7e0c727cb3871a312625ab7874665cd
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2026-01-22 16:13:15 +0000

    Apply fix for issue: Weak Encryption in credentials.c

--- libcli/auth/credentials.c
+++ libcli/auth/credentials.c
@@ -157,21 +157,13 @@ static NTSTATUS netlogon_creds_step_crypt(struct netlogon_creds_CredentialState 
 		if (!NT_STATUS_IS_OK(status)) {
 			return status;
 		}
-	} else if (creds->negotiate_flags & NETLOGON_NEG_ARCFOUR) {
-		memcpy(out->data, in->data, sizeof(out->data));
-
-		status = netlogon_creds_arcfour_crypt(creds,
-						      out->data,
-						      sizeof(out->data));
-		if (!NT_STATUS_IS_OK(status)) {
-			return status;
-		}
 	} else {
 		/*
-		 * DES encryption is no longer supported due to inadequate encryption strength.
-		 * Require AES or ARCFOUR encryption for secure communication.
+		 * Weak encryption algorithms (DES and RC4/ARCFOUR) are no longer
+		 * supported due to inadequate encryption strength.
+		 * Require AES encryption for secure communication.
 		 */
-		return NT_STATUS_NOT_SUPPORTED;
+		return NT_STATUS_DOWNGRADE_DETECTED;
 	}
 
 	return NT_STATUS_OK;
@@ -397,36 +389,20 @@ static NTSTATUS netlogon_creds_step(struct netlogon_creds_CredentialState *creds
 
 /*
   ARCFOUR encrypt/decrypt a password buffer using the session key
+  
+  DEPRECATED: RC4/ARCFOUR is cryptographically weak and should not be used.
+  This function is maintained only for compatibility with very old clients
+  and should not be called directly. Use AES encryption instead.
 */
 NTSTATUS netlogon_creds_arcfour_crypt(struct netlogon_creds_CredentialState *creds,
 				      uint8_t *data,
 				      size_t len)
 {
-	gnutls_cipher_hd_t cipher_hnd = NULL;
-	gnutls_datum_t session_key = {
-		.data = creds->session_key,
-		.size = sizeof(creds->session_key),
-	};
-	int rc;
-
-	rc = gnutls_cipher_init(&cipher_hnd,
-				GNUTLS_CIPHER_ARCFOUR_128,
-				&session_key,
-				NULL);
-	if (rc < 0) {
-		return gnutls_error_to_ntstatus(rc,
-						NT_STATUS_CRYPTO_SYSTEM_INVALID);
-	}
-	rc = gnutls_cipher_encrypt(cipher_hnd,
-				   data,
-				   len);
-	gnutls_cipher_deinit(cipher_hnd);
-	if (rc < 0) {
-		return gnutls_error_to_ntstatus(rc,
-						NT_STATUS_CRYPTO_SYSTEM_INVALID);
-	}
-
-	return NT_STATUS_OK;
+	/*
+	 * RC4/ARCFOUR is no longer supported due to inadequate encryption strength.
+	 * This function should not be called and will return an error.
+	 */
+	return NT_STATUS_NOT_SUPPORTED;
 }
 
 /*
@@ -1092,38 +1068,11 @@ static NTSTATUS netlogon_creds_crypt_samlogon_validation(struct netlogon_creds_C
 				return status;
 			}
 		}
-	} else if (creds->negotiate_flags & NETLOGON_NEG_ARCFOUR) {
-		/*
-		 * Don't crypt an all-zero key, it would give away
-		 * the NETLOGON pipe session key
-		 *
-		 * But for ServerAuthenticateKerberos we don't care
-		 * as we use a random key
-		 */
-		if (creds->authenticate_kerberos ||
-		    !all_zero(base->key.key, sizeof(base->key.key))) {
-			status = netlogon_creds_arcfour_crypt(creds,
-							      base->key.key,
-							      sizeof(base->key.key));
-			if (!NT_STATUS_IS_OK(status)) {
-				return status;
-			}
-		}
-
-		if (creds->authenticate_kerberos ||
-		    !all_zero(base->LMSessKey.key,
-			      sizeof(base->LMSessKey.key))) {
-			status = netlogon_creds_arcfour_crypt(creds,
-							      base->LMSessKey.key,
-							      sizeof(base->LMSessKey.key));
-			if (!NT_STATUS_IS_OK(status)) {
-				return status;
-			}
-		}
 	} else {
 		/*
-		 * DES encryption is no longer supported due to inadequate
-		 * encryption strength. Require at least ARCFOUR or AES.
+		 * Weak encryption algorithms (DES and RC4/ARCFOUR) are no longer
+		 * supported due to inadequate encryption strength.
+		 * Require AES encryption for secure communication.
 		 */
 		return NT_STATUS_DOWNGRADE_DETECTED;
 	}
@@ -1230,32 +1179,11 @@ static NTSTATUS netlogon_creds_crypt_samlogon_logon(struct netlogon_creds_Creden
 					return status;
 				}
 			}
-		} else if (creds->negotiate_flags & NETLOGON_NEG_ARCFOUR) {
-			uint8_t *h;
-
-			h = logon->password->lmpassword.hash;
-			if (!all_zero(h, 16)) {
-				status = netlogon_creds_arcfour_crypt(creds,
-								      h,
-								      16);
-				if (!NT_STATUS_IS_OK(status)) {
-					return status;
-				}
-			}
-
-			h = logon->password->ntpassword.hash;
-			if (!all_zero(h, 16)) {
-				status = netlogon_creds_arcfour_crypt(creds,
-								      h,
-								      16);
-				if (!NT_STATUS_IS_OK(status)) {
-					return status;
-				}
-			}
 		} else {
 			/*
-			 * DES encryption is no longer supported due to inadequate
-			 * encryption strength. Require at least ARCFOUR or AES.
+			 * Weak encryption algorithms (DES and RC4/ARCFOUR) are no longer
+			 * supported due to inadequate encryption strength.
+			 * Require AES encryption for secure communication.
 			 */
 			return NT_STATUS_DOWNGRADE_DETECTED;
 		}
@@ -1289,19 +1217,20 @@ static NTSTATUS netlogon_creds_crypt_samlogon_logon(struct netlogon_creds_Creden
 			if (!NT_STATUS_IS_OK(status)) {
 				return status;
 			}
-		} else if (creds->negotiate_flags & NETLOGON_NEG_ARCFOUR) {
-			status = netlogon_creds_arcfour_crypt(creds,
-							      logon->generic->data,
-							      logon->generic->length);
-			if (!NT_STATUS_IS_OK(status)) {
-				return status;
-			}
-		} else if (auth_level != DCERPC_AUTH_LEVEL_PRIVACY) {
+		} else if (auth_level == DCERPC_AUTH_LEVEL_PRIVACY) {
 			/*
-			 * Using DES to verify kerberos tickets makes no sense,
-			 * but if the connection is encrypted we don't care...
+			 * If the connection is encrypted at transport level,
+			 * we allow legacy clients, but weak ciphers are still
+			 * not used for Netlogon-level encryption.
 			 */
-			return NT_STATUS_INVALID_PARAMETER;
+			return NT_STATUS_OK;
+		} else {
+			/*
+			 * Weak encryption algorithms (DES and RC4/ARCFOUR) are no longer
+			 * supported due to inadequate encryption strength.
+			 * Require AES encryption or transport-level privacy.
+			 */
+			return NT_STATUS_DOWNGRADE_DETECTED;
 		}
 		break;
 
@@ -1432,21 +1361,21 @@ static NTSTATUS netlogon_creds_crypt_samr_CryptPassword(
 						  ARRAY_SIZE(pass->data));
 	}
 
-	if (creds->negotiate_flags & NETLOGON_NEG_ARCFOUR) {
-		return netlogon_creds_arcfour_crypt(creds,
-						    pass->data,
-						    ARRAY_SIZE(pass->data));
+	if (auth_level == DCERPC_AUTH_LEVEL_PRIVACY) {
+		/*
+		 * If the connection is encrypted at transport level,
+		 * we allow legacy clients, but weak ciphers are still
+		 * not used for Netlogon-level encryption.
+		 */
+		return NT_STATUS_OK;
 	}
 
 	/*
-	 * Using DES to verify to encrypt the password makes no sense,
-	 * but if the connection is encrypted we don't care...
+	 * Weak encryption algorithms (DES and RC4/ARCFOUR) are no longer
+	 * supported due to inadequate encryption strength.
+	 * Require AES encryption or transport-level privacy.
 	 */
-	if (auth_level != DCERPC_AUTH_LEVEL_PRIVACY) {
-		return NT_STATUS_INVALID_PARAMETER;
-	}
-
-	return NT_STATUS_OK;
+	return NT_STATUS_DOWNGRADE_DETECTED;
 }
 
 NTSTATUS netlogon_creds_decrypt_samr_CryptPassword(struct netlogon_creds_CredentialState *creds,
@@ -1508,21 +1437,21 @@ static NTSTATUS netlogon_creds_crypt_SendToSam(
 						  opaque_length);
 	}
 
-	if (creds->negotiate_flags & NETLOGON_NEG_ARCFOUR) {
-		return netlogon_creds_arcfour_crypt(creds,
-						    opaque_data,
-						    opaque_length);
+	if (auth_level == DCERPC_AUTH_LEVEL_PRIVACY) {
+		/*
+		 * If the connection is encrypted at transport level,
+		 * we allow legacy clients, but weak ciphers are still
+		 * not used for Netlogon-level encryption.
+		 */
+		return NT_STATUS_OK;
 	}
 
 	/*
-	 * Using DES to verify to encrypt the data makes no sense,
-	 * but if the connection is encrypted we don't care...
+	 * Weak encryption algorithms (DES and RC4/ARCFOUR) are no longer
+	 * supported due to inadequate encryption strength.
+	 * Require AES encryption or transport-level privacy.
 	 */
-	if (auth_level != DCERPC_AUTH_LEVEL_PRIVACY) {
-		return NT_STATUS_INVALID_PARAMETER;
-	}
-
-	return NT_STATUS_OK;
+	return NT_STATUS_DOWNGRADE_DETECTED;
 }
 
 NTSTATUS netlogon_creds_decrypt_SendToSam(struct netlogon_creds_CredentialState *creds,
