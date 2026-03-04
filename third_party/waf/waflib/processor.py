f5f48b7c12420789c4b430602f73135838d910dc
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2026-03-04 19:25:33 +0000

    Apply fix for issue: Unsafe Pickle Deserialization in processor.py at line 27

--- third_party/waf/waflib/processor.py
+++ third_party/waf/waflib/processor.py
@@ -2,11 +2,7 @@
 # encoding: utf-8
 # Thomas Nagy, 2016-2018 (ita)
 
-import os, sys, traceback, base64, signal
-try:
-	import cPickle
-except ImportError:
-	import pickle as cPickle
+import os, sys, traceback, base64, signal, json
 
 try:
 	import subprocess32 as subprocess
@@ -19,13 +15,38 @@ except AttributeError:
 	class TimeoutExpired(Exception):
 		pass
 
+def json_decode_subprocess_args(obj):
+	"""
+	Decode subprocess arguments from JSON serialization.
+	Handles special types like subprocess.DEVNULL, subprocess.PIPE, etc.
+	"""
+	if obj is None or isinstance(obj, (bool, int, str, float)):
+		return obj
+	elif isinstance(obj, dict):
+		if '__type__' in obj:
+			if obj['__type__'] == 'bytes':
+				return base64.b64decode(obj['value'])
+			elif obj['__type__'] == 'DEVNULL':
+				return subprocess.DEVNULL if hasattr(subprocess, 'DEVNULL') else None
+			elif obj['__type__'] == 'PIPE':
+				return subprocess.PIPE
+			elif obj['__type__'] == 'STDOUT':
+				return subprocess.STDOUT
+		else:
+			return {k: json_decode_subprocess_args(v) for k, v in obj.items()}
+	elif isinstance(obj, list):
+		return [json_decode_subprocess_args(item) for item in obj]
+	return obj
+
 def run():
 	txt = sys.stdin.readline().strip()
 	if not txt:
 		# parent process probably ended
 		sys.exit(18)
-	[cmd, kwargs, cargs] = cPickle.loads(base64.b64decode(txt))
-	cargs = cargs or {}
+	data = json.loads(base64.b64decode(txt).decode('utf-8'))
+	cmd = json_decode_subprocess_args(data[0])
+	kwargs = json_decode_subprocess_args(data[1])
+	cargs = json_decode_subprocess_args(data[2]) or {}
 
 	if not 'close_fds' in kwargs:
 		# workers have no fds
@@ -53,9 +74,15 @@ def run():
 		trace = str(cmd) + '\n' + ''.join(exc_lines)
 		ex = e.__class__.__name__
 
-	# it is just text so maybe we do not need to pickle()
-	tmp = [ret, out, err, ex, trace]
-	obj = base64.b64encode(cPickle.dumps(tmp))
+	# encode bytes as base64 strings for JSON serialization
+	tmp = [
+		ret,
+		base64.b64encode(out).decode('ascii') if out else None,
+		base64.b64encode(err).decode('ascii') if err else None,
+		ex,
+		trace
+	]
+	obj = base64.b64encode(json.dumps(tmp).encode('utf-8'))
 	sys.stdout.write(obj.decode())
 	sys.stdout.write('\n')
 	sys.stdout.flush()
