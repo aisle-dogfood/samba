d3048b87d6117e2967c939d24db0d113de53fabc
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2026-03-04 20:26:11 +0000

    Apply fix for issue: Subprocess Shell Injection in wintest.py at line 179

--- wintest/wintest.py
+++ wintest/wintest.py
@@ -175,12 +175,12 @@ class wintest():
             self.info('$ ' + " ".join(cmd))
         else:
             self.info('$ ' + cmd)
-        if output:
-            return subprocess.Popen([cmd], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=dir).communicate()[0]
         if isinstance(cmd, list):
             shell = False
         else:
             shell = True
+        if output:
+            return subprocess.Popen(cmd, shell=shell, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, cwd=dir).communicate()[0]
         if checkfail:
             return subprocess.check_call(cmd, shell=shell, cwd=dir)
         else:
