d06f99d18089288a365b8a50cc384b7debb261c2
weareaisle-demo-app <Iv23linzHARThdPwQevh+weareaisle-demo-app@users.noreply.github.com>
2026-03-30 17:31:39 +0000

    Apply fix for issue: Improper Null Termination in substitute.c

--- source3/lib/substitute.c
+++ source3/lib/substitute.c
@@ -269,6 +269,9 @@ void standard_sub_basic(const char *smb_name, const char *domain_name,
 
 	if ( (s = talloc_sub_basic(talloc_tos(), smb_name, domain_name, str )) != NULL ) {
 		strncpy( str, s, len );
+		if (len > 0) {
+			str[len - 1] = '\0';
+		}
 	}
 
 	TALLOC_FREE( s );
